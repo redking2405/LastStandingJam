0c54b0becd07df8788b5bbac3bac83eb5b0369d0
Leonis26 <leonis2616@gmail.com>
2019-09-09 20:46:32 +0200

    AI Behaviour
    - Random moves with cooldowns
    - Resetting to a new random move upon other character or wall encounter within 180° range

--- LastJamStanding/Assets/Scripts/Script_Leonard/AIBehaviour.cs
+++ LastJamStanding/Assets/Scripts/Script_Leonard/AIBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace UnityStandardAssets._2D
 {
     [RequireComponent(typeof(Character2D))]
@@ -11,6 +12,8 @@ namespace UnityStandardAssets._2D
 
         // Randomizers
         float r_movestop, r_speed, r_orientation;
+        bool obstacleReached = false;
+        bool moving = false;
 
         // Player reference
         [SerializeField]
@@ -23,24 +26,56 @@ namespace UnityStandardAssets._2D
 
         void Start()
         {
-            r_movestop = Random.Range(1, 5); // the cooldown between moves
+            r_movestop = Random.Range(1, 5); // the cooldown between two moves
             r_speed = Random.Range(1, 3);    // the speed when moving the next time
         }
 
-        // Update is called once per frame
         void Update()
         {
-            if (r_movestop == 0)
+            if (r_movestop <= 0) // Resets random values (cooldown/speed/orientation)
             {
-                r_movestop = Random.Range(1, 5);
-                r_speed = Random.Range(1, 3);
-                r_orientation = Random.Range(0, 360);
+                SetNextMoveState(Random.Range(0, 360));
             }
-            else
+            else // Moves until the cooldown resets
             {
-                m_NPC.Move(r_speed, r_orientation);
                 r_movestop -= Time.deltaTime;
             }
+
+            ContactFilter2D wallContactFilter2D = new ContactFilter2D();
+            ContactFilter2D charaContactFilter2D = new ContactFilter2D();
+            wallContactFilter2D.SetLayerMask(LayerMask.NameToLayer("Wall"));
+            charaContactFilter2D.SetLayerMask(LayerMask.NameToLayer("Character"));
+            List<Collider2D> colliderResults = new List<Collider2D>();
+
+            // Collision detections (walls having priority over other characters)
+            if (Physics2D.OverlapCircle(transform.position, 1.5f, wallContactFilter2D, colliderResults) > 0) // Touching a wall (static walls only, going to it)
+            {
+                SetRotationRangeUponEncounter(colliderResults);
+            }
+            else if (Physics2D.OverlapPoint(transform.position + transform.right * 1.5f, charaContactFilter2D, colliderResults) > 0) // Touching obstacles in front of it (base vector is pointing to the right (rot 0)
+            {
+                SetRotationRangeUponEncounter(colliderResults);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (moving)
+                m_NPC.Move(r_speed, r_orientation);
+        }
+
+        void SetNextMoveState(float orientation)
+        {
+            moving = !moving;
+            r_movestop = Random.Range(0.5f, 5);
+            r_speed = Random.Range(1, 3);
+        }
+
+        void SetRotationRangeUponEncounter(List<Collider2D>_colliderResults)
+        {
+            Vector2 obstacleToCharacterNormal = Vector2.Perpendicular(_colliderResults[0].transform.position - transform.position);
+            float minimumAngle = Vector2.Angle(Vector2.right, obstacleToCharacterNormal);
+            SetNextMoveState(Random.Range(minimumAngle, (minimumAngle + 180) % 360));
         }
     }
 }
